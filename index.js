71b79569f720fa928a389f9d95804aab72af0c6e
Vivek Kalmath <vivek.kalmath2001@gmail.com>
2021-08-25 15:54:20 +0530

    updated visits to the web app

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb+srv://tiger:scotch@cluster0.yrmd4.mongodb.net/urlShortner?retryWrites=true&w=majority',{useNewUrlParser:true , useUnifiedTopology: true}).then(()=>{
     console.log("connected with mongodb atlas");
-    console.log(process.env.DB_HOST);
 }).catch(err => console.log(err));
 
 app.use(express.urlencoded({extended : "false"}));
